33c37b7b5a1c2b61ef0d07f8ea6e58ecede0d678
Hwayoung Yoon <ghkdud204@gmail.com>
2021-04-05 18:13:29 +0900

    Add files via upload

--- ch05.R
+++ ch05.R
@@ -12,11 +12,6 @@ attach(sweat)
 x <- sweat[,2:4]
 x
 
-# 그림 5.2.
-install.packages("scatterplot3d")
-library(scatterplot3d)
-scatterplot3d(x, pch="*", main="3d scatterplot")
-
 # for ginv
 library(MASS)
 p <- ncol(x); p
@@ -41,6 +36,11 @@ ggplot(sweat, aes(x1,x2)) +
   geom_point() +                          # + : not in the front
   stat_ellipse(type="norm", level=0.90)   # 90% default=multivariate n
 
+# 3차원 산점도
+install.packages("scatterplot3d")
+library(scatterplot3d)
+scatterplot3d(x, pch="*", main="3d scatterplot")
+
 
 #####################################
 ##### 예제 5.2. H0: male mean vector = female mean vector
@@ -54,36 +54,125 @@ xm <- d[gender==1, 2:5]; xm
 ## female data
 xf <- d[gender==2, 2:5]; xf
 
+sm <- cov(xm); sm
+sf <- cov(xf); sf
+xmbar <- colMeans(xm); xmbar
+xfbar <- colMeans(xf); xfbar
+n1 <- nrow(xm); n1
+n2 <- nrow(xf); n2
+S <- ((n1-1)*sm+(n2-1)*sf)/(n1+n2-2)
+p <- 4
+
+library(MASS)
+T2 <- t(xmbar-xfbar) %*% ginv((1/n1+1/n2)*S) %*% (xmbar-xfbar); T2
+f <- ((n1+n2-p)/(n1+n2-p-1)*p)*T2; f
+pvalue <- 1-pf(f, p, n1+n2-p-1); pvalue
 
 
+#####################################
+##### 연습문제 5.2. obs (x1, x2)' 2*1 column vector
+
+# 데이터 입력
+x <- rbind(c(2,12), c(8,9), c(6,9), c(8,10)); x
+
+n <- dim(x)[[1]]; n
+p <- dim(x)[[2]]; p
+plot(x[,1], x[,2], pch=19, cex=2, col="blue")
+xbar <- colMeans(x); xbar
+S <- cov(x); S
+sinv <- solve(S); sinv
+
+# mean vector under H0: mu=mu0
+mu0 <- c(7,11); mu0
+T2 <- n*t(xbar-mu0) %*% solve(S) %*% (xbar-mu0); T2
+f <- ((n-p)/(n-1)*p)*T2; f
+pvalue <- 1-pf(f, p, n-p); pvalue
+pv <- pf(f, p, n-p, lower.tail=F); pv
+
+C <- rbind(c(1,-1), c(1,1)); C
+
+# subjectwise
+C %*% x[1,]
+C %*% x[2,]
+C %*% x[3,]
+C %*% x[4,]
+cx <- rbind(t(C %*% x[1,]), t(C %*% x[2,]), 
+            t(C %*% x[3,]), t(C %*% x[4,])); cx
+
+# mean vector under H0
+cmu0 <- C %*% mu0; cmu0
+
+cxbar <- colMeans(cx); cxbar
+cS <- cov(cx); cS
+
+library(MASS)
+cT2 <- n*t(cxbar-cmu0) %*% ginv(cS) %*% (cxbar-cmu0); cT2
+cf <- ((n-p)/(n-1)*p)*cT2; cf
+cpvalue <- 1-pf(cf, p, n-p); cpvalue
 
 
+#####################################
+##### 연습문제 5.5.
+
+# 데이터 입력
+library(readxl)
+A <- read_xlsx("data/practice/Table5.9_EngineerPilot.xlsx")
+A <- as.data.frame(A); A
+attach(A)
+eng <- A[(group=="E"), 1:2]; eng
+pil <- A[(group=="P"), 1:2]; pil
+
+# (a).
+ebar <- colMeans(eng); ebar
+s1 <- cov(eng); s1
+pbar <- colMeans(pil); pbar
+s2 <- cov(pil); s2
+
+## + : not in the front
+library(ggplot2)
+ggplot(A, aes(A[,1], A[,2], pch=group)) +
+  geom_point() +
+  stat_ellipse(aes(A[,1], A[,2], color=group), type="norm", level=0)
+
+## another draw
+install.packages("devtools")
+install.packages("proto")
+install.packages("plotly")
+library(ggplot2)
+library(devtools)
+library(proto)
+library(plotly)
+
+p <- ggplot(data=A, aes(x=A[,1], y=A[,2])) +
+  geom_point() +
+  stat_ellipse(geom="polygon", alpha=0.5, aes(fill=group))
+fig <- ggplotly(p); fig
+
+# (b). 두 집단의 공분산이 같을 때 두 집단의 모평균벡터가 같은가
+n1 <- nrow(eng); n1
+n2 <- nrow(pil); n2
+p <- ncol(A[,1:2]); p
+spool <- ((n1-1)*s1+(n2-1)*s2)/(n1+n2-2); spool
+
+library(MASS)
+T2 <- (n1*n2/(n1+n2))*t(ebar-pbar) %*% ginv(spool) %*% (ebar-pbar); spool
+f <- ((n1+n2-p-1)/((n1+n2-2)*p))*T2; f
+pvalue <- 1-pf(f, p, n1+n2-p-1); pvalue
+
+# (c). 두 집단의 공분산이 다를 때 두 집단의 모평균벡터가 같은가
+library(MASS)
+T2_2 <- t(ebar-pbar) %*% ginv(s1/n1+s2/n2) %*% (ebar-pbar); T2_2
+## n1-p, n2-p가 충분히 클 때
+p.value <- pchisq(T2_2, p, lower.tail=F); p.value
+
+# manova(6장)
+fit <- manova(cbind(A[,1], A[,2]) ~ group)
+summary(fit)
 
 
+#####################################
+##### 연습문제 5.10.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 공분산행렬 동일성 검정
+# 반응변수 개수 p=4
+# 설명변수의 범주 개수 g=2
