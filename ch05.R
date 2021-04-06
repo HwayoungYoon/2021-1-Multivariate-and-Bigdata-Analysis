c11aa9f72fa33bc7662a9e0a39bc09ac990ccda1
Hwayoung Yoon <ghkdud204@gmail.com>
2021-04-06 14:42:22 +0900

    Add files via upload

--- ch05.R
+++ ch05.R
@@ -6,35 +6,36 @@ setwd("C:/R/Multivariate and Bigdata Analysis")
 ##### 예제 5.2. Hotelling T^2 test
 
 # 데이터 입력
-sweat <- read.csv("data/sweat.csv", header=T)
-sweat
+sweat <- read.csv("data/sweat.csv", header=T); sweat
 attach(sweat)
-x <- sweat[,2:4]
-x
+x <- sweat[,2:4]; x
 
-# for ginv
-library(MASS)
 p <- ncol(x); p
 xbar <- apply(x, 2, mean); xbar
 colMeans(x)
 S <- cov(x); S
 n <- dim(x)[[1]]; n
-# mean vector under H0
+
+# 일집단 Hotelling T^2 test
+library(MASS)
 mu0 <- c(4,50,10); mu0
 T2 <- n*t(xbar-mu0) %*% ginv(S) %*% (xbar-mu0); T2
 f <- ((n-p)/(n-1)*p)*T2; f
 pvalue <- 1-pf(f, p, n-p); pvalue
 
+# 동시 신뢰영역
 library(ggplot2)
+## 95%, multivariate t
 ggplot(sweat, aes(x1,x2)) +
-  geom_point() +    # + : not in the front
-  stat_ellipse()    # 95% default=multivariate t
+  geom_point() +
+  stat_ellipse()
+## 95%, multivariate n
+  geom_point() +
+  stat_ellipse(type="norm")
+## 90%, multivariate n
 ggplot(sweat, aes(x1,x2)) +
-  geom_point() +              # + : not in the front
-  stat_ellipse(type="norm")   # 95% default=multivariate t
-ggplot(sweat, aes(x1,x2)) +
-  geom_point() +                          # + : not in the front
-  stat_ellipse(type="norm", level=0.90)   # 90% default=multivariate n
+  geom_point() +
+  stat_ellipse(type="norm", level=0.90)
 
 # 3차원 산점도
 install.packages("scatterplot3d")
@@ -63,6 +64,7 @@ n2 <- nrow(xf); n2
 S <- ((n1-1)*sm+(n2-1)*sf)/(n1+n2-2)
 p <- 4
 
+# 두 모집단에 대한 Hotelling T^2 test
 library(MASS)
 T2 <- t(xmbar-xfbar) %*% ginv((1/n1+1/n2)*S) %*% (xmbar-xfbar); T2
 f <- ((n1+n2-p)/(n1+n2-p-1)*p)*T2; f
@@ -70,43 +72,52 @@ pvalue <- 1-pf(f, p, n1+n2-p-1); pvalue
 
 
 #####################################
-##### 연습문제 5.2. obs (x1, x2)' 2*1 column vector
+##### 연습문제 5.1. obs (x1, x2)' 2*1 column vector
 
 # 데이터 입력
 x <- rbind(c(2,12), c(8,9), c(6,9), c(8,10)); x
 
 n <- dim(x)[[1]]; n
 p <- dim(x)[[2]]; p
+
+# scatter plot
 plot(x[,1], x[,2], pch=19, cex=2, col="blue")
+
+# (a).
 xbar <- colMeans(x); xbar
+
+# (b).
 S <- cov(x); S
+
+# (c).
 sinv <- solve(S); sinv
 
-# mean vector under H0: mu=mu0
+# (d). mean vector under H0: mu=mu0
 mu0 <- c(7,11); mu0
 T2 <- n*t(xbar-mu0) %*% solve(S) %*% (xbar-mu0); T2
 f <- ((n-p)/(n-1)*p)*T2; f
 pvalue <- 1-pf(f, p, n-p); pvalue
 pv <- pf(f, p, n-p, lower.tail=F); pv
 
+# (e).
 C <- rbind(c(1,-1), c(1,1)); C
-
-# subjectwise
 C %*% x[1,]
 C %*% x[2,]
 C %*% x[3,]
 C %*% x[4,]
 cx <- rbind(t(C %*% x[1,]), t(C %*% x[2,]), 
             t(C %*% x[3,]), t(C %*% x[4,])); cx
+C %*% t(x)
 
-# mean vector under H0
+# (f). mean vector under H0
 cmu0 <- C %*% mu0; cmu0
-
 cxbar <- colMeans(cx); cxbar
 cS <- cov(cx); cS
 
+# (g).
 library(MASS)
-cT2 <- n*t(cxbar-cmu0) %*% ginv(cS) %*% (cxbar-cmu0); cT2
+cT2 <- n*t(cxbar-cmu0) %*% ginv(cS) %*% (cxbar-cmu0)
+cT2; T2
 cf <- ((n-p)/(n-1)*p)*cT2; cf
 cpvalue <- 1-pf(cf, p, n-p); cpvalue
 
@@ -128,7 +139,7 @@ s1 <- cov(eng); s1
 pbar <- colMeans(pil); pbar
 s2 <- cov(pil); s2
 
-## + : not in the front
+## 동시 신뢰영역
 library(ggplot2)
 ggplot(A, aes(A[,1], A[,2], pch=group)) +
   geom_point() +
@@ -171,7 +182,7 @@ summary(fit)
 
 
 #####################################
-##### 연습문제 5.10.
+##### 연습문제 5.7.
 
 # 공분산행렬 동일성 검정
 # 반응변수 개수 p=4
