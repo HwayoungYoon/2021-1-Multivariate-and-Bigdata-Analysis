af39ad40446c6eafc1c97809c583aa5ed59e0bc1
Hwayoung Yoon <ghkdud204@gmail.com>
2021-04-17 23:19:39 +0900

    Add files via upload

--- ch06_ex.R
+++ ch06_ex.R
@@ -46,9 +46,41 @@ n <- n1+n2+n3
 # 6.7.a
 
 # 6.7.b
+method <- factor(method)
+y <- cbind(x1, x2, x3, x4)
+fit <- manova(y ~ method)
+B <- (n-1)*cov(fit$fitted.values); B
+E <- (n-1)*cov(fit$residuals); E
+B+E
 
 # 6.7.c
+summary(fit, test="Wilks")
+summary(fr, test="Pillai")
+summary(fr, test="Roy")
+summary(fr, test="Hotelling")
 
 # 6.7.d
+summary(aov(x1 ~ method))
+summary(aov(x2 ~ method))
+summary(aov(x3 ~ method))
+summary(aov(x4 ~ method))
 
 # 6.7.e
+p <- 4; g <- 3
+v1 <- n1-1; v2 <- n2-1; v3 <- n3-1
+v <- c(v1,v2,v3)
+
+s1 <- cov(fc1); s2 <- cov(fc2); s3 <- cov(fc3)
+spool <- ((n1-1)*s1+(n2-1)*s2+(n3-1)*s3)/(n1+n2+n3-3)
+
+M1 <- (det(s1)/det(spool))^((n1-1)/2)
+M2 <- (det(s2)/det(spool))^((n2-1)/2)
+M3 <- (det(s3)/det(spool))^((n3-1)/2)
+M <- M1*M2*M3
+
+s1; s2; s3; spool
+
+temp <- (2*p^2+3*p-1)/(6*(p+1)*(g-1))
+c1 <- (sum(1/v)-1/sum(v))*temp
+u <- -2*(1-c1)*log(M); u
+pchisq(u, (g-1)*p*(p+1)/2, lower.tail=F)
