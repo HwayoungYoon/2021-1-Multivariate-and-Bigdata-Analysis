9fc0851829437bd46e5600e7398388ead28fefaa
Hwayoung Yoon <ghkdud204@gmail.com>
2021-04-25 14:29:59 +0900

    Add files via upload

--- ch07_ex.R
+++ ch07_ex.R
@@ -5,8 +5,9 @@ setwd("C:/R/Multivariate and Bigdata Analysis")
 ########## 7.2
 
 
-
 ########## 7.9
+S <- matrix(c(1,0.71,0.58,0.56,0.65,0.71,1,0.71,0.6,0.69,0.58,0.71,1,0.75,
+              0.71,0.56,0.6,0.75,1,0.74,0.65,0.69,0.71,0.74,1), nc=5)
 
 # 7.9.a
 
@@ -15,6 +16,10 @@ setwd("C:/R/Multivariate and Bigdata Analysis")
 
 
 ########## 7.11
+library(readxl)
+Hemo <- read_xlsx("data/practice/Table7.5_Blood.xlsx")
+Hemo <- as.data.frame(Hemo)
+attach(Hemo)
 
 # 7.11.a
 
