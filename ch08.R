ae769d4ae72e06cf475a8d82b07a0d91f1e03540
Hwayoung Yoon <ghkdud204@gmail.com>
2021-05-08 17:26:49 +0900

    Add files via upload

--- ch08.R
+++ ch08.R
@@ -49,41 +49,58 @@ plot(fact3$loadings[,1], fact3$loadings[,2], pch=16, xlab="factor1", ylab="facto
   text(x=fact3$loadings[,1], y=fact2$loadings[,2], labels=namevar, adj=0)
   abline(v=0, h=0)
 plot(fact3$loadings[,1], fact3$loadings[,2], pch=16, xlab="factor1", ylab="factor3", main="factor pattern")
-  text(x=fact2$loadings[,1], y=fact2$loadings[,3], labels=namevar, adj=0)
-  abline(v=0, h=0)
+text(x=fact2$loadings[,1], y=fact2$loadings[,3], labels=namevar, adj=0)
+abline(v=0, h=0)
 
 
+#####################################
+##### 연습문제 8.8. 46명 당뇨병 환자
+d <- read.csv ("data/practice/Table9.2_Diabetes.csv", header=T)
+head(d)
+attach(d)
+x <- d[,2:6]; x 
 
+# (b) 주성분법 이용 인자
+p.cor <- princomp(x, cor=T)
+summary(p.cor)
+p.cor$loadings 
 
+# Gradient Projection Rrotation for factor anal
+install.packages ("GPArotation")
+library (psych)
+library (GPArotation) 
 
+# 주성분법
+x_factor <- principal(x, nfactors=2, rotate="none", cor="cor")
+x_factor 
 
+library (graphics)
+screeplot (p.cor, npcs=5, type="lines", main="screeplot-correlation") 
 
+# factor scores scatterplot
+factor1 <- p.cor$scores[,1]
+factor2 <- p.cor$scores [,2]
+plot (factor1, factor2, xlab="factor1", ylab="factor2")
+# or
+plot(x_factor$scores[,1], x_factor$scores[,2], pch="*",
+     xlab="factor1", ylab="factor2", main="factor scores") 
 
+# factot loadings plot
+a <- names(p.cor$loadings[,2]) <- c("Y1", "Y2", "X1", "X2", "X3")
+plot(p.cor$loadings[,1], p.cor$loadings[,2], pch=19, col="blue",
+     xlab="factor1", ylab="factor2", main="factor loadings")
+  text(p.cor$loadings[,1], p.cor$loadings[,2], labels=a, adj=o, cex=1) 
 
+# varimax rotation
+fact_var <- factanal(x, factors=2, rotation="varimax")
+fact_var 
 
+# maximum likelihood : default
+fact_ml <- factanal(x, factors=2, rotation="none")
+fact_ml 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# GPA rotation
+TV <- GPFoblq(loadings(fact_ml), method="oblimin", normalize=T)
+print(TV)
+print(TV, table=T)
+summary(TV)
